714a6699276f7ab58ff8898b01b16dc41c34cbe2
Riccardo Soldan <riccardosoldan@hotmail.it>
2021-05-11 14:17:13 +0100

    added Figure2c

--- script/script.R
+++ script/script.R
@@ -2,6 +2,8 @@ library(tidyverse)
 library(readxl)
 source("~/theme_publication.R")
 
+#### FIG 2b ####
+
 fig2b <- read_excel("../data/supp_tables_dataset_AgroLux_paper_v2.xlsx", sheet = 4, skip = 7, n_max = 49) %>% 
   group_by(Treatments, `Leaf*`) %>% 
   summarise(Mean=mean(`Mean**`)) 
@@ -10,10 +12,12 @@ fig2b %>%
   group_by(Treatments) %>% 
   summarise(N=n(),  sd=sd(Mean), se=sd/sqrt(N), Mean=mean(Mean), ) %>% 
   mutate(se=se/1000, Mean=Mean/1000) %>% 
+  mutate(Treatments = fct_relevel(Treatments, 
+                            "WT + WT(GFP)", "LUX + WT(GFP)", "WT + LUX(GFP)")) %>%
 ggplot(aes(Treatments, Mean)) +
-  geom_bar(stat="identity")+
-  geom_errorbar(aes(ymin=Mean-se, ymax=Mean+se),width=0.2)+
-  geom_jitter(data=fig2b, aes(Treatments, Mean/1000), size=4, alpha=0.6, width = 0.1)+
+  geom_bar(stat="identity", width = 0.6, color="black", fill="darkgreen", alpha=0.5)+
+  geom_errorbar(aes(ymin=Mean-se, ymax=Mean+se),width=0.5)+
+  geom_jitter(data=fig2b, aes(Treatments, Mean/1000), size=3, alpha=0.6, width = 0.1)+
   theme_Publication(base_size = 12)+
   ylab("GFP (RFUx1000)")+
   theme(axis.text.x = element_text(angle = 45, hjust=1))
@@ -21,21 +25,27 @@ ggplot(aes(Treatments, Mean)) +
 ggsave("../results/fig2b.svg", width = 7, height = 12, units = "cm")
 
 
+#### FIG 2C ####
 
-fig2b <- read_excel("../data/supp_tables_dataset_AgroLux_paper_v2.xlsx", sheet = 4, skip = 7, n_max = 49) %>% 
+fig2c <- read_excel("../data/supp_tables_dataset_AgroLux_paper_v2.xlsx", sheet = 5, skip = 7, n_max = 49) %>% 
   group_by(Treatments, `Leaf*`) %>% 
-  summarise(Mean=mean(`Mean**`)) 
+  summarise(Mean=mean(`Mean – Background**`, na.rm = TRUE)) 
 
-fig2b %>% 
+fig2c %>% 
   group_by(Treatments) %>% 
-  summarise(N=n(),  sd=sd(Mean), se=sd/sqrt(N), Mean=mean(Mean), ) %>% 
-  mutate(se=se/1000, Mean=Mean/1000) %>% 
+  summarise(N=n(),  sd=sd(Mean), se=sd/sqrt(N), Mean=mean(Mean)) %>% 
+  mutate(se=se, Mean=Mean) %>% 
+  mutate(Treatments = fct_relevel(Treatments, 
+                                  "WT + WT(GFP)", "LUX + WT(GFP)", "WT + LUX(GFP)")) %>%
   ggplot(aes(Treatments, Mean)) +
-  geom_bar(stat="identity")+
-  geom_errorbar(aes(ymin=Mean-se, ymax=Mean+se),width=0.2)+
-  geom_jitter(data=fig2b, aes(Treatments, Mean/1000), size=4, alpha=0.6, width = 0.1)+
+  geom_bar(stat="identity", width = 0.6, color="black", fill="red", alpha=0.5)+
+  geom_errorbar(aes(ymin=Mean-se, ymax=Mean+se),width=0.5)+
+  geom_jitter(data=fig2c, aes(Treatments, Mean), size=3, alpha=0.6, width = 0.1)+
   theme_Publication(base_size = 12)+
-  ylab("GFP (RFUx1000)")+
+  ylab("Luminescence (RLU)")+
   theme(axis.text.x = element_text(angle = 45, hjust=1))
 
-ggsave("../results/fig2b.svg", width = 7, height = 12, units = "cm")
+ggsave("../results/fig2c.svg", width = 7, height = 12, units = "cm")
+
+
+#### FIG 
