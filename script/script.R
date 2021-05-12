fc27a9ba2d03ef10d56f17300c72001d666819fc
Riccardo Soldan <riccardosoldan@hotmail.it>
2021-05-12 09:39:10 +0100

    added figure4b

--- script/script.R
+++ script/script.R
@@ -4,6 +4,8 @@ source("~/theme_publication.R")
 
 #### FIG 2b ####
 
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
 fig2b <- read_excel("../data/supp_tables_dataset_AgroLux_paper_v2.xlsx", sheet = 4, skip = 7, n_max = 49) %>% 
   group_by(Treatments, `Leaf*`) %>% 
   summarise(Mean=mean(`Mean**`)) 
@@ -48,4 +50,65 @@ fig2c %>%
 ggsave("../results/fig2c.svg", width = 7, height = 12, units = "cm")
 
 
-#### FIG 
+#### FIG 4b ####
+
+fig4b <- read_excel("../data/supp_tables_dataset_AgroLux_paper_v2.xlsx", sheet = 8, skip = 7, n_max = 37) %>%
+  select(-Mean) %>% 
+ rename(Mean=`Mean – Background**`) 
+
+fig4b %>% 
+  group_by(Treatments, dpi) %>% 
+  summarise(N=n(),  sd=sd(Mean), se=sd/sqrt(N), Mean=mean(Mean)) %>% 
+  ggplot(aes(Treatments, Mean)) +
+  geom_bar(stat="identity", width = 0.6, color="black", fill="red", alpha=0.5)+
+  geom_errorbar(aes(ymin=Mean-se, ymax=Mean+se),width=0.5)+
+  geom_jitter(data=fig4b, aes(Treatments, Mean), size=3, alpha=0.6, width = 0.1)+
+  theme_Publication(base_size = 12)+
+  ylab("Luminescence (RLU)")+
+  facet_wrap(~dpi) +
+  theme(axis.text.x = element_text(angle = 45, hjust=1))
+
+ggsave("../results/fig4b.svg", width = 10, height = 12, units = "cm")
+
+
+# Groups for statistical significant
+
+c_2dpi <- fig4b %>% 
+  filter(Treatments=="Cf4+Avr4", dpi==2) %>% 
+  select(Mean)
+
+t1_2dpi <-  fig4b %>% 
+  filter(Treatments=="Avr4", dpi==2) %>% 
+  select(Mean)
+
+t2_2dpi <-  fig4b %>% 
+  filter(Treatments=="Cf4", dpi==2) %>% 
+  select(Mean)
+
+c_5dpi <- fig4b %>% 
+  filter(Treatments=="Cf4+Avr4", dpi==5) %>% 
+  select(Mean)
+
+t1_5dpi <-  fig4b %>% 
+  filter(Treatments=="Avr4", dpi==5) %>% 
+  select(Mean)
+
+t2_5dpi <-  fig4b %>% 
+  filter(Treatments=="Cf4", dpi==5) %>% 
+  select(Mean)
+
+# significant. p.value 0.001. If we correct with Bonferroni, the critical p.value would be 0.025
+
+t.test(as.vector(c_2dpi), as.vector(t1_2dpi))
+
+# significant. p.value 0.009. If we correct with Bonferroni, the critical p.value would be 0.025
+
+t.test(as.vector(c_2dpi), as.vector(t2_2dpi))
+
+# significant. p.value 0.0002. If we correct with Bonferroni, the critical p.value would be 0.025
+
+t.test(as.vector(c_5dpi), as.vector(t1_5dpi))
+
+# significant. p.value 0.0006. If we correct with Bonferroni, the critical p.value would be 0.025
+
+t.test(as.vector(c_5dpi), as.vector(t2_5dpi))
