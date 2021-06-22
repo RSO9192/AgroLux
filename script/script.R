d915e6b71d5a49d6352bc719f0698bf643d8c6cd
Riccardo Soldan <riccardosoldan@hotmail.it>
2021-06-22 13:11:34 +0100

    Added tables with p.values and figure 5a-b

--- script/script.R
+++ script/script.R
@@ -2,7 +2,9 @@ library(tidyverse)
 library(readxl)
 source("~/theme_publication.R")
 
-#### FIG 2b ####
+
+# figure 2B ---------------------------------------------------------------
+
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
@@ -28,7 +30,10 @@ ggsave("../results/fig2b.pdf", width = 7, height = 12, units = "cm")
 ggsave("../results/fig2b.svg", width = 7, height = 12, units = "cm")
 
 
-#### FIG 2C ####
+
+# figure 2C ---------------------------------------------------------------
+
+
 
 fig2c <- read_excel("../data/supp_tables_dataset_AgroLux_paper_v2.xlsx", sheet = 5, skip = 7, n_max = 49) %>% 
   group_by(Treatments, `Leaf*`) %>% 
@@ -52,7 +57,10 @@ ggsave("../results/fig2c.pdf", width = 7, height = 12, units = "cm")
 ggsave("../results/fig2c.svg", width = 7, height = 12, units = "cm")
 
 
-#### FIG 4b ####
+
+# figure 4b ---------------------------------------------------------------
+
+
 
 fig4b <- read_excel("../data/supp_tables_dataset_AgroLux_paper_v2.xlsx", sheet = 8, skip = 7, n_max = 37) %>%
   select(-Mean) %>% 
@@ -115,3 +123,119 @@ t.test(as.vector(c_5dpi), as.vector(t1_5dpi))
 # significant. p.value 0.0006. If we correct with Bonferroni, the critical p.value would be 0.025
 
 t.test(as.vector(c_5dpi), as.vector(t2_5dpi))
+
+p.value <- data.frame(Comparison=rep(c("Control-Avr4", "Control-Cf4"), 2), 
+                      DPI=rep(c(2,5), each=2), p.value=c(0.001, 0.009, 0.0002, 0.0006))
+
+write_csv(p.value, "../results/pvalues_figure4b.csv")
+
+
+# figure 5A ---------------------------------------------------------------
+
+
+fig5a <- read_excel("../data/supp_tables_dataset_AgroLux_paper_v2.xlsx", sheet = 11, skip = 7, n_max=49) %>%
+  select(-Mean) %>% 
+  rename(Mean=`Mean – Background**`) 
+
+fig5a %>% 
+  group_by(Treatments, dpi) %>% 
+  summarise(N=n(),  sd=sd(Mean), se=sd/sqrt(N), Mean=mean(Mean)) %>% 
+  ggplot(aes(Treatments, Mean)) +
+  geom_bar(stat="identity", width = 0.6, color="black", fill="red", alpha=0.5)+
+  geom_errorbar(aes(ymin=Mean-se, ymax=Mean+se),width=0.5)+
+  geom_jitter(data=fig5a, aes(Treatments, Mean), size=3, alpha=0.6, width = 0.1)+
+  theme_Publication(base_size = 12)+
+  ylab("Luminescence (RLU)")+
+  theme(axis.text.x = element_text(angle = 45, hjust=1))+
+  coord_flip()+
+  facet_wrap(~dpi, ncol = 1) 
+
+ggsave("../results/fig5a.pdf", width = 10, height = 12, units = "cm")
+ggsave("../results/fig5a.svg", width = 10, height = 12, units = "cm")
+
+
+# Groups for statistical significance
+
+c1_2dpi <- fig5a %>% 
+  filter(Treatments=="Cf4/Avr4", dpi==2) %>% 
+  select(Mean)
+
+c2_2dpi <- fig5a %>% 
+  filter(Treatments=="Cf9/Avr9", dpi==2) %>% 
+  select(Mean)
+
+t1_2dpi <-  fig5a %>% 
+  filter(Treatments=="Cf9/Avr4", dpi==2) %>% 
+  select(Mean)
+
+t2_2dpi <-  fig5a %>% 
+  filter(Treatments=="Cf4/Avr9", dpi==2) %>% 
+  select(Mean)
+
+
+c1_5dpi <- fig5a %>% 
+  filter(Treatments=="Cf4/Avr4", dpi==5) %>% 
+  select(Mean)
+
+c2_5dpi <- fig5a %>% 
+  filter(Treatments=="Cf9/Avr9", dpi==5) %>% 
+  select(Mean)
+
+t1_5dpi <-  fig5a %>% 
+  filter(Treatments=="Cf9/Avr4", dpi==5) %>% 
+  select(Mean)
+
+t2_5dpi <-  fig5a %>% 
+  filter(Treatments=="Cf4/Avr9", dpi==5) %>% 
+  select(Mean)
+
+
+# Comparisison Cf4/Avr4-Cf4/Avr9 2dpi. significant. p.value 6.211e-09. If we correct with Bonferroni, the critical p.value would be 0.025
+
+t.test(as.vector(c1_2dpi), as.vector(t2_2dpi))
+
+# Comparisison Cf9/Avr9-Cf9/Avr4 2dpi. significant. p.value 3.782e-07. If we correct with Bonferroni, the critical p.value would be 0.025
+
+t.test(as.vector(c2_2dpi), as.vector(t1_2dpi))
+
+# Comparisison Cf4/Avr4-Cf4/Avr9 5dpi. significant. p.value 0.0004221. If we correct with Bonferroni, the critical p.value would be 0.025
+
+t.test(as.vector(c1_5dpi), as.vector(t2_5dpi))
+
+# Comparisison Cf9/Avr9-Cf9/Avr4 5dpi. significant. p.value 0.0003414. If we correct with Bonferroni, the critical p.value would be 0.025
+
+t.test(as.vector(c2_5dpi), as.vector(t1_5dpi))
+
+
+p.value <- data.frame(Comparison=rep(c("Cf4/Avr4-Cf4/Avr9", "Cf9/Avr9-Cf9/Avr4"), 2), 
+                      DPI=rep(c(2,5), each=2), p.value=c(6.211e-09, 3.782e-07, 0.0004221, 0.0003414))
+
+
+write_csv(p.value, "../results/pvalues_figure5a.csv")
+
+
+
+# figure 5B ---------------------------------------------------------------
+
+fig5b <- read_excel("../data/supp_tables_dataset_AgroLux_paper_v2.xlsx", sheet = 12, skip = 7, n_max=49) %>%
+  select(-Mean) %>% 
+  rename(Mean=`Mean – Background**`) 
+
+fig5b %>% 
+  group_by(Treatments, dpi) %>% 
+  summarise(N=n(),  sd=sd(Mean), se=sd/sqrt(N), Mean=mean(Mean)) %>% 
+  ggplot(aes(Treatments, Mean)) +
+  geom_bar(stat="identity", width = 0.6, color="black", fill="red", alpha=0.5)+
+  geom_errorbar(aes(ymin=Mean-se, ymax=Mean+se),width=0.5)+
+  geom_jitter(data=fig5b, aes(Treatments, Mean), size=3, alpha=0.6, width = 0.1)+
+  theme_Publication(base_size = 12)+
+  ylab("Luminescence (RLU)")+
+  theme(axis.text.x = element_text(angle = 45, hjust=1))+
+  coord_flip()+
+  facet_wrap(~dpi, ncol = 1) 
+
+ggsave("../results/fig5b.pdf", width = 10, height = 12, units = "cm")
+ggsave("../results/fig5b.svg", width = 10, height = 12, units = "cm")
+
+
+
