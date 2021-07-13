b0517a37b372b1374e9026c94c2f8002834bf054
Riccardo Soldan <riccardosoldan@hotmail.it>
2021-07-13 09:21:13 +0100

    updated raw data and script

--- script/script.R
+++ script/script.R
@@ -10,10 +10,10 @@ source("~/theme_publication.R")
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-fig2b <-
+fig3b <-
   read_excel(
-    "../data/supp_tables_dataset_AgroLux_paper_v2.xlsx",
-    sheet = 4,
+    "../data/supp_tables_dataset_AgroLux_paper_v3.xlsx",
+    sheet = 5,
     skip = 7,
     n_max = 49
   ) %>%
@@ -25,7 +25,7 @@ fig2b <-
                              "WT + WT(GFP)", "LUX + WT(GFP)", "WT + LUX(GFP)")
   )
 
-fig2b %>%
+fig3b %>%
   group_by(Treatments) %>%
   summarise(
     N = n(),
@@ -45,38 +45,48 @@ fig2b %>%
   ) +
   geom_errorbar(aes(ymin = Mean - se, ymax = Mean + se), width = 0.5) +
   geom_jitter(
-    data = fig2b,
+    data = fig3b,
     aes(Treatments, Mean / 1000),
     size = 3,
     alpha = 0.6,
     width = 0.1
   ) +
   theme_Publication(base_size = 12) +
-  ylab("GFP (RFUx1000)") +
+  ylab("Fluorescence (RFU x 1000)") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 ggsave(
-  "../results/fig2b.pdf",
+  "../results/fig3b.pdf",
   width = 7,
   height = 12,
   units = "cm"
 )
 ggsave(
-  "../results/fig2b.svg",
+  "../results/fig3b.svg",
   width = 7,
   height = 12,
   units = "cm"
 )
 
+# statistics
+
+mod_3b <- lm(Mean~Treatments, data=fig3b)
+
+plot(mod_3b)
+
+anova(mod_3b)
+
+stat_3b <- c(" Non significant. [F(2,21)=0.0091, p=0.99]")
+
+write.table(stat_3b, file = "../results/stat_fig3b.txt")
+
+# figure 3C ---------------------------------------------------------------
 
 
-# figure 2C ---------------------------------------------------------------
-
-
-fig2c <-
+fig3c <-
   read_excel(
-    "../data/supp_tables_dataset_AgroLux_paper_v2.xlsx",
-    sheet = 5,
+    "../data/supp_tables_dataset_AgroLux_paper_v3.xlsx",
+    sheet = 6,
     skip = 7,
     n_max = 49
   ) %>%
@@ -88,7 +98,7 @@ fig2c <-
                              "WT + WT(GFP)", "LUX + WT(GFP)", "WT + LUX(GFP)")
   )
 
-fig2c %>%
+fig3c %>%
   group_by(Treatments) %>%
   summarise(
     N = n(),
@@ -107,7 +117,7 @@ fig2c %>%
   ) +
   geom_errorbar(aes(ymin = Mean - se, ymax = Mean + se), width = 0.3) +
   geom_jitter(
-    data = fig2c,
+    data = fig3c,
     aes(Treatments, Mean),
     size = 3,
     alpha = 0.6,
@@ -118,26 +128,38 @@ fig2c %>%
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 ggsave(
-  "../results/fig2c.pdf",
+  "../results/fig3c.pdf",
   width = 7,
   height = 12,
   units = "cm"
 )
 ggsave(
-  "../results/fig2c.svg",
+  "../results/fig3c.svg",
   width = 7,
   height = 12,
   units = "cm"
 )
 
 
+# statistics
 
-# figure 3c ---------------------------------------------------------------
+mod_3c<- lm(Mean~Treatments, data=fig3c)
 
-fig3c <-
+plot(mod_3c)
+
+anova(mod_3c)
+
+stat_3c <- c(" Non significant. [F(1,14)=0.4321, p=0.52]")
+
+write.table(stat_3c, file = "../results/stat_fig3c.txt")
+
+
+# figure 4c ---------------------------------------------------------------
+
+fig4c <-
   read_excel(
-    "../data/supp_tables_dataset_AgroLux_paper_v2.xlsx",
-    sheet = 6,
+    "../data/supp_tables_dataset_AgroLux_paper_v3.xlsx",
+    sheet = 7,
     skip = 7,
     n_max = 200
   ) %>%
@@ -151,7 +173,7 @@ fig3c <-
   )
 
 
-fig3c %>%
+fig4c %>%
   group_by(Treatments, dpi) %>%
   summarise(
     N = n(),
@@ -171,7 +193,7 @@ fig3c %>%
     position = position_dodge(width = 0.9)
   ) +
   geom_point(
-    data = fig3c,
+    data = fig4c,
     aes(dpi, Mean, color = Treatments),
     color = "black",
     position = position_dodge(width = 0.9),
@@ -185,26 +207,27 @@ fig3c %>%
 
 
 ggsave(
-  "../results/fig3c.pdf",
-  width = 10,
-  height = 12,
-  units = "cm"
-)
-ggsave(
-  "../results/fig3c.svg",
+  "../results/fig4c.pdf",
   width = 10,
   height = 12,
   units = "cm"
 )
 
-# statistical significance at each dpis
+ggsave(
+  "../results/fig4c.svg",
+  width = 10,
+  height = 12,
+  units = "cm"
+)
+
+# statistical significance at each DPIs
 
 mod_dpis <- map(1:4, function(x) {
-  fig3c.mod <- fig3c %>%
+  fig4c.mod <- fig4c %>%
     filter(dpi == x) %>%
     mutate(Treatments = as.factor(Treatments))
   
-  dpi_1_aov <- gls(Mean ~ Treatments, data = fig3c.mod)
+  dpi_1_aov <- gls(Mean ~ Treatments, data = fig4c.mod)
   
   anova(dpi_1_aov) []
   
@@ -224,16 +247,16 @@ p.value <-
     )
   )
 
-write_csv(p.value, "../results/pvalues_fig3c.csv")
+write_csv(p.value, "../results/pvalues_fig4c.csv")
 
 
-# figure 3d ---------------------------------------------------------------
+# figure 4d ---------------------------------------------------------------
 
 
-fig3d <-
+fig4d <-
   read_excel(
-    "../data/supp_tables_dataset_AgroLux_paper_v2.xlsx",
-    sheet = 7,
+    "../data/supp_tables_dataset_AgroLux_paper_v3.xlsx",
+    sheet = 8,
     skip = 7,
     n_max = 200
   ) %>%
@@ -246,7 +269,7 @@ fig3d <-
     Treatments = fct_relevel(Treatments, "Low", "Medium", "High")
   )
 
-fig3d %>%
+fig4d %>%
   group_by(Treatments, dpi) %>%
   summarise(
     N = n(),
@@ -266,7 +289,7 @@ fig3d %>%
     position = position_dodge(width = 0.9)
   ) +
   geom_point(
-    data = fig3d,
+    data = fig4d,
     aes(dpi, Mean, color = Treatments),
     color = "black",
     position = position_dodge(width = 0.9),
@@ -282,13 +305,14 @@ fig3d %>%
 
 
 ggsave(
-  "../results/fig3d.pdf",
+  "../results/fig4d.pdf",
   width = 10,
   height = 12,
   units = "cm"
 )
+
 ggsave(
-  "../results/fig3d.svg",
+  "../results/fig4d.svg",
   width = 10,
   height = 12,
   units = "cm"
@@ -298,16 +322,16 @@ ggsave(
 # model comparison at each dpi
 
 comp.mod <- map(1:4, function(x) {
-  fig3d.mod <- fig3d %>%
+  fig4d.mod <- fig4d %>%
     filter(dpi == x) %>%
     mutate(Treatments = as.factor(Treatments))
   
-  dpi_aov <- gls(Mean ~ Treatments, data = fig3d.mod)
+  dpi_aov <- gls(Mean ~ Treatments, data = fig4d.mod)
   
   # accounting for non-heterogeneity
   vs <- varIdent(form = ~ 1 | Treatments)
   dpi_aov_het <-
-    gls(Mean ~ Treatments, data = fig3d.mod, weights = vs)
+    gls(Mean ~ Treatments, data = fig4d.mod, weights = vs)
   
   print(anova(dpi_aov, dpi_aov_het))
   
@@ -329,24 +353,24 @@ p.value <-
     )
   )
 
-write_csv(p.value, "../results/pvalues_fig3d.csv")
+write_csv(p.value, "../results/pvalues_fig4d.csv")
 
 
 
-# figure 4b ---------------------------------------------------------------
+# figure 5b ---------------------------------------------------------------
 
 
-fig4b <-
+fig5b <-
   read_excel(
-    "../data/supp_tables_dataset_AgroLux_paper_v2.xlsx",
-    sheet = 8,
+    "../data/supp_tables_dataset_AgroLux_paper_v3.xlsx",
+    sheet = 9,
     skip = 7,
     n_max = 37
   ) %>%
   select(-Mean) %>%
   rename(Mean = `Mean – Background**`)
 
-fig4b %>%
+fig5b %>%
   group_by(Treatments, dpi) %>%
   summarise(
     N = n(),
@@ -364,7 +388,7 @@ fig4b %>%
   ) +
   geom_errorbar(aes(ymin = Mean - se, ymax = Mean + se), width = 0.5) +
   geom_jitter(
-    data = fig4b,
+    data = fig5b,
     aes(Treatments, Mean),
     size = 3,
     alpha = 0.6,
@@ -391,27 +415,27 @@ ggsave(
 
 # Groups for statistical significance
 
-c_2dpi <- fig4b %>%
+c_2dpi <- fig5b %>%
   filter(Treatments == "Cf4+Avr4", dpi == 2) %>%
   select(Mean)
 
-t1_2dpi <-  fig4b %>%
+t1_2dpi <-  fig5b %>%
   filter(Treatments == "Avr4", dpi == 2) %>%
   select(Mean)
 
-t2_2dpi <-  fig4b %>%
+t2_2dpi <-  fig5b %>%
   filter(Treatments == "Cf4", dpi == 2) %>%
   select(Mean)
 
-c_5dpi <- fig4b %>%
+c_5dpi <- fig5b %>%
   filter(Treatments == "Cf4+Avr4", dpi == 5) %>%
   select(Mean)
 
-t1_5dpi <-  fig4b %>%
+t1_5dpi <-  fig5b %>%
   filter(Treatments == "Avr4", dpi == 5) %>%
   select(Mean)
 
-t2_5dpi <-  fig4b %>%
+t2_5dpi <-  fig5b %>%
   filter(Treatments == "Cf4", dpi == 5) %>%
   select(Mean)
 
@@ -438,23 +462,24 @@ p.value <-
     p.value = c(0.001, 0.009, 0.0002, 0.0006)
   )
 
-write_csv(p.value, "../results/pvalues_figure4b.csv")
+write_csv(p.value, "../results/pvalues_figure5b.csv")
 
 
-# figure 5A ---------------------------------------------------------------
+# figure 6B ---------------------------------------------------------------
 
 
-fig5a <-
+fig6b <-
   read_excel(
-    "../data/supp_tables_dataset_AgroLux_paper_v2.xlsx",
-    sheet = 11,
+    "../data/supp_tables_dataset_AgroLux_paper_v3.xlsx",
+    sheet = 12,
     skip = 7,
     n_max = 49
   ) %>%
   select(-Mean) %>%
   rename(Mean = `Mean – Background**`)
 
-fig5a %>%
+fig6b %>%
+  mutate(Mean=Mean/100) %>% 
   group_by(Treatments, dpi) %>%
   summarise(
     N = n(),
@@ -472,26 +497,25 @@ fig5a %>%
   ) +
   geom_errorbar(aes(ymin = Mean - se, ymax = Mean + se), width = 0.5) +
   geom_jitter(
-    data = fig5a,
-    aes(Treatments, Mean),
+    data = fig6b,
+    aes(Treatments, Mean/100),
     size = 3,
     alpha = 0.6,
     width = 0.1
   ) +
   theme_Publication(base_size = 12) +
-  ylab("Luminescence (RLU)") +
+  ylab("Luminescence (RLU X 100)") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  coord_flip() +
-  facet_wrap( ~ dpi, ncol = 1)
+  facet_wrap( ~ dpi, ncol = 2)
 
 ggsave(
-  "../results/fig5a.pdf",
+  "../results/fig6b.pdf",
   width = 10,
   height = 12,
   units = "cm"
 )
 ggsave(
-  "../results/fig5a.svg",
+  "../results/fig6b.svg",
   width = 10,
   height = 12,
   units = "cm"
@@ -500,36 +524,36 @@ ggsave(
 
 # Groups for statistical significance
 
-c1_2dpi <- fig5a %>%
+c1_2dpi <- fig6b %>%
   filter(Treatments == "Cf4/Avr4", dpi == 2) %>%
   select(Mean)
 
-c2_2dpi <- fig5a %>%
+c2_2dpi <- fig6b %>%
   filter(Treatments == "Cf9/Avr9", dpi == 2) %>%
   select(Mean)
 
-t1_2dpi <-  fig5a %>%
+t1_2dpi <-  fig6b %>%
   filter(Treatments == "Cf9/Avr4", dpi == 2) %>%
   select(Mean)
 
-t2_2dpi <-  fig5a %>%
+t2_2dpi <-  fig6b %>%
   filter(Treatments == "Cf4/Avr9", dpi == 2) %>%
   select(Mean)
 
 
-c1_5dpi <- fig5a %>%
+c1_5dpi <- fig6b %>%
   filter(Treatments == "Cf4/Avr4", dpi == 5) %>%
   select(Mean)
 
-c2_5dpi <- fig5a %>%
+c2_5dpi <- fig6b %>%
   filter(Treatments == "Cf9/Avr9", dpi == 5) %>%
   select(Mean)
 
-t1_5dpi <-  fig5a %>%
+t1_5dpi <-  fig6b %>%
   filter(Treatments == "Cf9/Avr4", dpi == 5) %>%
   select(Mean)
 
-t2_5dpi <-  fig5a %>%
+t2_5dpi <-  fig6b %>%
   filter(Treatments == "Cf4/Avr9", dpi == 5) %>%
   select(Mean)
 
@@ -561,23 +585,24 @@ p.value <-
   )
 
 
-write_csv(p.value, "../results/pvalues_figure5a.csv")
+write_csv(p.value, "../results/pvalues_figure6b.csv")
 
 
 
-# figure 5B ---------------------------------------------------------------
+# figure 6c ---------------------------------------------------------------
 
-fig5b <-
+fig6c <-
   read_excel(
-    "../data/supp_tables_dataset_AgroLux_paper_v2.xlsx",
-    sheet = 12,
+    "../data/supp_tables_dataset_AgroLux_paper_v3.xlsx",
+    sheet = 13,
     skip = 7,
     n_max = 49
   ) %>%
   select(-Mean) %>%
   rename(Mean = `Mean – Background**`)
 
-fig5b %>%
+fig6c %>%
+  mutate(Mean=Mean/1000) %>% 
   group_by(Treatments, dpi) %>%
   summarise(
     N = n(),
@@ -595,7 +620,116 @@ fig5b %>%
   ) +
   geom_errorbar(aes(ymin = Mean - se, ymax = Mean + se), width = 0.5) +
   geom_jitter(
-    data = fig5b,
+    data = fig6c,
+    aes(Treatments, Mean/1000),
+    size = 3,
+    alpha = 0.6,
+    width = 0.1
+  ) +
+  theme_Publication(base_size = 12) +
+  ylab("Fluorescence (RLU X 1000)") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  facet_wrap( ~ dpi, ncol = 2)
+
+
+# Groups for statistical significance
+
+c1_2dpi <- fig6c %>%
+  filter(Treatments == "Cf4/Avr4", dpi == 2) %>%
+  select(Mean)
+
+c2_2dpi <- fig6c %>%
+  filter(Treatments == "Cf9/Avr9", dpi == 2) %>%
+  select(Mean)
+
+t1_2dpi <-  fig6c %>%
+  filter(Treatments == "Cf9/Avr4", dpi == 2) %>%
+  select(Mean)
+
+t2_2dpi <-  fig6c %>%
+  filter(Treatments == "Cf4/Avr9", dpi == 2) %>%
+  select(Mean)
+
+
+c1_5dpi <- fig6c %>%
+  filter(Treatments == "Cf4/Avr4", dpi == 5) %>%
+  select(Mean)
+
+c2_5dpi <- fig6c %>%
+  filter(Treatments == "Cf9/Avr9", dpi == 5) %>%
+  select(Mean)
+
+t1_5dpi <-  fig6c %>%
+  filter(Treatments == "Cf9/Avr4", dpi == 5) %>%
+  select(Mean)
+
+t2_5dpi <-  fig6c %>%
+  filter(Treatments == "Cf4/Avr9", dpi == 5) %>%
+  select(Mean)
+
+
+# Comparisison Cf4/Avr4-Cf4/Avr9 2dpi. If we correct with Bonferroni, the critical p.value would be 0.025
+
+t.test(as.vector(c1_2dpi), as.vector(t2_2dpi))
+
+# Comparisison Cf9/Avr9-Cf9/Avr4 2dpi. If we correct with Bonferroni, the critical p.value would be 0.025
+
+t.test(as.vector(c2_2dpi), as.vector(t1_2dpi))
+
+# Comparisison Cf4/Avr4-Cf4/Avr9 5dpi. If we correct with Bonferroni, the critical p.value would be 0.025
+
+t.test(as.vector(c1_5dpi), as.vector(t2_5dpi))
+
+# Comparisison Cf9/Avr9-Cf9/Avr4 5dpi.  If we correct with Bonferroni, the critical p.value would be 0.025
+
+t.test(as.vector(c2_5dpi), as.vector(t1_5dpi))
+
+p.value <-
+  data.frame(
+    Comparison = rep(c(
+      "Cf4/Avr4-Cf4/Avr9", "Cf9/Avr9-Cf9/Avr4"
+    ), 2),
+    DPI = rep(c(2, 5), each = 2),
+    p.value = c( 0.03105,  0.0005889,  1.336e-06,  2.191e-07)
+  )
+
+
+write_csv(p.value, "../results/pvalues_fig6c.csv")
+
+
+
+# figure 7b ---------------------------------------------------------------
+
+
+fig7b <-
+  read_excel(
+    "../data/supp_tables_dataset_AgroLux_paper_v3.xlsx",
+    sheet = 14,
+    skip = 7,
+    n_max = 49
+  ) %>%
+  select(-Mean) %>%
+  rename(Mean = `Mean – Background**`)
+
+fig7b %>%
+  group_by(Treatments, dpi) %>%
+  summarise(
+    N = n(),
+    sd = sd(Mean),
+    se = sd / sqrt(N),
+    Mean = mean(Mean)
+  ) %>%
+  ggplot(aes(Treatments, Mean)) +
+  geom_bar(
+    stat = "identity",
+    width = 0.6,
+    color = "black",
+    fill = "red",
+    alpha = 0.5
+  ) +
+  geom_errorbar(aes(ymin = Mean - se, ymax = Mean + se), width = 0.5) +
+  geom_jitter(
+    data = fig7b,
     aes(Treatments, Mean),
     size = 3,
     alpha = 0.6,
@@ -604,17 +738,17 @@ fig5b %>%
   theme_Publication(base_size = 12) +
   ylab("Luminescence (RLU)") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  coord_flip() +
-  facet_wrap( ~ dpi, ncol = 1)
+  facet_wrap( ~ dpi, ncol = 2)
 
 ggsave(
-  "../results/fig5b.pdf",
+  "../results/fig7b.pdf",
   width = 10,
   height = 12,
   units = "cm"
 )
+
 ggsave(
-  "../results/fig5b.svg",
+  "../results/fig7b.svg",
   width = 10,
   height = 12,
   units = "cm"
